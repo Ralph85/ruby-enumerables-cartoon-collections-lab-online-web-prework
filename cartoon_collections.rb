21f51849efcd053c956727b40b841e3f0d4e1258
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-27 00:53:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,10 @@ end
 
 long_planeteer_calls(call_screams)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type| cheese_types.include?(type)
+  end
 end
+
+find_the_cheese(snacks)
