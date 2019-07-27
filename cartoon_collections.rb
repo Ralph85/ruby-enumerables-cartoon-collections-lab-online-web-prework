1fa9c167f43b1a001105e2852e93d08cf5ea7099
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-27 00:49:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,13 +8,27 @@ end
 
 roll_call_dwarves
 
-def summon_captain_planet(planeteer_calls)
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  while i < array.length
+    new_array << array[i].capitalize + "!"
+    i += 1
+  end
+  new_array
+end
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i = i + 1
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+long_planeteer_calls(call_screams)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
